b11df7cc4f8a43e68656c98b00477f32cfcf5c83
IsmaailAitGaghou <aitgaghou17@gmail.com>
2025-08-12 00:33:35 +0100

    Challenge 2

--- week-1/day-1/daily-challenges/challenge-2.py
+++ week-1/day-1/daily-challenges/challenge-2.py
@@ -3,7 +3,7 @@ users_word = str(input("Enter a word: "))
 new_users_word = ""
 
 for i in range(len(users_word)):
-    if i == 0:
+    if new_users_word == "" :
         new_users_word += users_word[i]
     elif users_word[i] != users_word[i - 1]:
         new_users_word += users_word[i]
