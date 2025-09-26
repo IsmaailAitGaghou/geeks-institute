45b9522d12024aaaec8ae35144c3a715669caa77
IsmaailAitGaghou <aitgaghou17@gmail.com>
2025-09-26 10:28:12 +0100

    adding some changes

--- week-5/day-4/exercises-xp-ninja/weather-dashboard/weather.js
+++ week-5/day-4/exercises-xp-ninja/weather-dashboard/weather.js
@@ -1,11 +1,38 @@
 import axios from 'axios';
-const getData = async (cityName) => {
+import dotenv from 'dotenv';
+dotenv.config();
+
+const API_KEY = process.env.API_KEY;
+
+const getCity = async (cityName) => {
+    
+
     try {
-        const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q=${cityName}`);
+        const response = await axios.get(
+          `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${API_KEY}`
+        );
         console.log(response.data);
     } catch (error) {
         console.error('Error fetching weather data:', error);
     }
 }
 
-getData();
+const getCoordinates = async (cityName) => {
+    const cityData = await getCity(cityName);
+    if (cityData && cityData.length > 0) {
+        const { lat, lon } = cityData[0];
+        return { lat, lon };
+    }
+    return null;
+};
+
+const getWeather = async (lat, lon) => {
+    try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+        );
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }  
+}
